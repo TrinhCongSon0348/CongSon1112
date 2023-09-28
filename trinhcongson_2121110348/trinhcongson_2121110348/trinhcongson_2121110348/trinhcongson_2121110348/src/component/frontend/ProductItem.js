012251a734b0fe007176963d9a27efea012a57db
TrinhCongSon0348 <trinhcongson0348@gmail.com>
2023-09-28 16:31:59 +0700

    Them Project tuan 3

---
+++ trinhcongson_2121110348/trinhcongson_2121110348/trinhcongson_2121110348/trinhcongson_2121110348/src/component/frontend/ProductItem.js
@@ -0,0 +1,36 @@
+import { urlImage } from "../../config";
+import { Link } from "react-router-dom";
+
+function ProductItem(props) {
+    return (
+        <div className="col-md-3 mb-3">
+            <div className="product-item border">
+                <div className="product-image">
+                    <Link to={"/chi-tiet-san-pham/" + props.product.slug}>
+                        <img src={urlImage + "product/" + props.product.image} className="img-fluid" alt={props.product.image} />
+                    </Link>
+                </div>
+                <div className="product-name p-2">
+                    <h3 className="text-center text-danger fs-6">
+                        <Link to={"/chi-tiet-san-pham/" + props.product.slug}>
+                            {props.product.name}
+                        </Link>
+                    </h3>
+                </div>
+                <div className="product-price p-2">
+                    <div className="row">
+                        <div className="col-6">
+                            <strong className="text-danger fs-3">{props.product.price_sale}<sup>$</sup></strong>
+                        </div>
+                        <div className="col-6 text-end">
+                            <del className="fs-5">{props.product.price}</del><sup>$</sup>
+                        </div>
+                    </div>
+                </div>
+                <div className="link-detail">Chi tiết</div>
+            </div>
+        </div>
+    );
+}
+
+export default ProductItem;
---
+++ trinhcongson_2121110348/trinhcongson_2121110348/trinhcongson_2121110348/trinhcongson_2121110348/src/services/PostService.js
@@ -0,0 +1,31 @@
+
+import httpAxios from "../httpAxios";
+
+function getAll()
+{
+    return httpAxios.get('post/index');
+}
+function getById(id)
+{
+    return httpAxios.get('post/show/'+id);
+}
+function create(data)
+{
+    return httpAxios.post('post/store',data);
+}
+function update(id,data)
+{
+    return httpAxios.post('post/update/'+id,data);
+}
+function remove(id)
+{
+    return httpAxios.delete('post/destroy/'+id);
+}
+const postservice={
+    getAll:getAll,
+    getById:getById,
+    create:create,
+    update:update,
+    remove:remove
+}
+export default postservice;
